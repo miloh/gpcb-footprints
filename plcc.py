c77f858eff36f70ad826e292ebb9fc95f82d97ae
Ronald Miloh Alexander <miloh@froggytoad.net>
2015-03-03 08:58:39 -0800

    doing everything i tell it to do -- this footprint in progress is just...

--- plcc.py
+++ plcc.py
@@ -13,13 +13,13 @@ f.pitch = 3.3
 f.height = 3.4 
 f.width = 2.4
 f.silkwidth = 0.254 
-f.padheight = 1.6
-f.padwidth = 0.9
+f.padheight =0.9 
+f.padwidth = 1.6 
 f.pinswide= 3.4 
 #   silk component outlines
 f.rowofpads([-f.height/2 - f.padwidth/2, 0],"down",1,2)
 f.rowofpads([f.height/2 + f.padwidth/2 , 0],"up",3,4)
 # Pin1 diamond mark with 10mil silkwidth
-f.silk_diamond(-(f.pitch*(f.pins-1))/2,f.width+f.padwidth,f.padheight,0.254)
-#f.silk_crop(3.3, pin1="diamond",rotate='-90')
+#f.silk_diamond(-(f.pitch*(f.pins-1))/2,f.width+f.padwidth,f.padheight,0.254)
+f.silk_crop(3.3, pin1="diamond",rotate='-90')
 f.finish()
