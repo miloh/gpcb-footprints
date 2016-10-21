ce9b6a5ed152a5067c52c4cfa19295a016eae12e
R. Miloh Alexander <miloh@froggytoad.net>
2016-10-21 12:58:04 -0700

    cleanup whitespaces

--- d2pak.py
+++ d2pak.py
@@ -2,7 +2,6 @@
 # R. Miloh Alexander
 # Use: unrestricted with attribution
 
-
 import footgen
 
 # package index:
@@ -23,9 +22,9 @@ f.epadwidth = f.width # mm
 f.epadheight = 8.89 # mm
 f.padwidth = 2.08  # mm
 f.padheight = 3.81 # mm
-f.add_pad( str(1), (-f.width + f.padwidth)/2, f.height-(f.padheight)/2, f.padwidth, f.padheight) 
-f.add_pad( str(2), 0.0, f.epadheight/2,                    f.epadwidth, f.epadheight) 
-f.add_pad( str(3), (f.width - f.padwidth)/2, f.height-(f.padheight)/2, f.padwidth, f.padheight) 
+f.add_pad( str(1), (-f.width + f.padwidth)/2, f.height-(f.padheight)/2, f.padwidth, f.padheight)
+f.add_pad( str(2), 0.0, f.epadheight/2,                    f.epadwidth, f.epadheight)
+f.add_pad( str(3), (f.width - f.padwidth)/2, f.height-(f.padheight)/2, f.padwidth, f.padheight)
 
 f.finish()
 
